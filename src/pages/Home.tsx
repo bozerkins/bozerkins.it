31b02d438a06898a8e257e91cb2f77b4fb86b079
Bogdans Ozerkins <b.ozerkins@outlook.com>
2025-05-10 15:04:27 -0700

    simplify and clean-up home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <div className="home">
-    <section className="header-section">
-      <div className="header-content">
-        <h1>Welcome to My Cozy Den!</h1>
-        <div className="blog-link">
-          <Link to="/blog" className="blog-button show-desktop">Visit My Blog</Link>
-          <Link to="/blog" className="blog-button show-mobile">Blog</Link>
-        </div>
-      </div>
-      <img src="./profile_pic_pixel.png" alt="Profile" className="profile-picture" />
-    </section>
-      <section className="about">
-        <div className="about-text">
-          <h2>About Me</h2>
-          <p>Hello! I’m Bogdans, and I'm a software engineer. </p>
-          <p>I started my journey in software development back in the 6th grade and took my first steps into professional development in grade 11. It’s been a fun and rewarding ride so far!</p>
-          <p>With over a decade of industry experience, I have a pasion for crafting quality software and designing efficient architectures. I believe in the importance of readable codebases and the power of data, analytics, logs, and metrics to drive decisions.</p>
-          <p>I love helping coders become better coders.</p>
-          <p>I love delivering what customers need most and seeing the delight in peoples eyes.</p>
-          <p>My moto is: doing less with more.</p>
-          <div className="social-links">
-            <a href="https://github.com/bozerkins" target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-            <a href="https://www.linkedin.com/in/bozerkins/" target="_blank" rel="noopener noreferrer">
-              LinkedIn
-            </a>
+      <section className="header-section">
+        <div className="header-content">
+          <h1>About me</h1>
+          <div className="about-text">
+            <p>Hello! I'm Bogdans, and I'm a software enthusiast. </p>
+            <p>I started my journey in software development back in the 6th grade and took my first steps into professional development in grade 11. It's been a fun and rewarding ride so far!</p>
+            <p>With over a decade of industry experience, I have a pasion for crafting quality software and designing efficient architectures. I believe in the importance of readable codebases and the power of data, analytics, logs, and metrics to drive decisions.</p>
+            <p><strong>My moto is:</strong> doing less with more.</p>
+            <div className="social-links">
+              <a href="https://github.com/bozerkins" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+              <a href="https://www.linkedin.com/in/bozerkins/" target="_blank" rel="noopener noreferrer">
+                LinkedIn
+              </a>
+            </div>
           </div>
         </div>
-    </section>
-
+        <img src="./profile_pic_pixel.png" alt="Profile" className="profile-picture" />
+      </section>
       <section className="projects">
-        <h2>Recent Projects</h2>
+        <h2>Recent OSS</h2>
         <div className="project">
           <h3>Explore Tables</h3>
           <p>React library to render pivot tables for in-house data analysis and ad-hoc reports.</p>
@@ -62,27 +51,13 @@ const Home: React.FC = () => {
           </a>
         </div>
         <div className="project">
-          <h3>Clickhouse PHP Client</h3>
+          <h3>Clickhouse Client</h3>
           <p>A client to call ClickHouse database written in PHP.</p>
           <p><strong>What's Cool:</strong> One of the first PHP clients for ClickHouse.</p>
           <a href="https://github.com/bozerkins/clickhouse-client" target="_blank" rel="noopener noreferrer">
             View on GitHub
           </a>
         </div>
-      </section>
-
-      <section className="hobbies">
-        <h2>Hobbies & Passions</h2>
-        <ul>
-          <li>My lovely wife {"<3"}</li>
-          <li>Photography</li>
-          <li>Traveling</li>
-          <li>Swimming</li>
-          <li>Digital Art</li>
-          <li>Video Games</li>
-          <li>Biking</li>
-          <li>Netflix</li>
-        </ul>
       </section>
     </div>
   );
